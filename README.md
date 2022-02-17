41532a8cfdcea9adaa1fa8f6641381b684ecd16e
TanPhan1112 <minhtan111293@gmail.com>
2022-02-17 00:42:38 -0800

    Updated PORT

--- README.md
+++
@@ -1,1 +0,0 @@
-# MERN-Essential-Training-Backend
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import routes from './routes/soccerRoutes';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // mongo connection
 mongoose.Promise = global.Promise;
